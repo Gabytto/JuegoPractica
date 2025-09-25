98c5f67f4694ce6e614ccae29d008d7a84866c34
Gabytto <gabyttox@gmail.com>
2025-09-25 10:54:37 -0300

    Pociones, consumir pocion, recibir daño

--- Assets/Scripts/movimiento.cs
+++ Assets/Scripts/movimiento.cs
@@ -6,23 +6,28 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
-    // Variables para movimiento del pj
+    // Propiedades del pj
     private Rigidbody2D rb;
+    private int vidaInicial = 100;
+    private int vidaActual;
+    private int dañoRecibido = 20;
+    private int curaVida = 20;
+    // Variables para movimiento del pj
     private int movHorizontal;
     private int movVertical;
     private Vector2 mov;
     // Variables sprintar
-    [SerializeField]private float speed;
+    [SerializeField] private float speed;
     private float ogSpeed;
     private float mulSpeed;
     [SerializeField] private float valMulSpeed = 1.5f;
     // Variables modo rage
     public bool rageOn = false;
-    public float daño = 10f;   
+    public float daño = 10f;
     private float ogDaño;
     private float mulDaño;
     // Variables timer
-    [SerializeField]private timer timer;
+    [SerializeField] private timer timer;
     // Inventario
     public int rocasObtenidas;
 
@@ -34,15 +39,16 @@ public class movimiento : MonoBehaviour
         mulSpeed = speed * valMulSpeed;
         ogDaño = daño;
         mulDaño = daño * 5f;
+        vidaActual = vidaInicial;
     }
-    
+
     void Update()
-    {   
+    {
         // Sprintar
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             speed = mulSpeed;
-        }    
+        }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             speed = ogSpeed;
@@ -71,16 +77,16 @@ public class movimiento : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             movHorizontal = 1;
-        }    
-        else if(Input.GetKey(KeyCode.A))
+        }
+        else if (Input.GetKey(KeyCode.A))
         {
             movHorizontal = -1;
-        }    
+        }
         else { movHorizontal = 0; }
 
         // Movimiento vertical del personaje
         if (Input.GetKey(KeyCode.W))
-        { movVertical = 1;}
+        { movVertical = 1; }
         else if (Input.GetKey(KeyCode.S))
         { movVertical = -1; }
         else { movVertical = 0; }
@@ -96,6 +102,32 @@ public class movimiento : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.collider.CompareTag("enemy"))
+        {
+            RecibirDaño();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("potaVida"))
+        {
+            RecibirCuracion();
+        }
+    }
+    private void RecibirDaño()
+    {
+        vidaActual -= dañoRecibido;
         Debug.Log("Auch!!!");
+        Debug.Log("Tu vida actual es " + vidaActual);
+        if (vidaActual <= 0)
+        {
+            gameObject.SetActive(false);
+            Debug.Log("Te has muerto.");
+        }
+    }
+    private void RecibirCuracion()
+    {
+        vidaActual += curaVida;
+        Debug.Log("Tu vida actual es " + vidaActual);
     }
 }
