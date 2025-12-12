b0643b6b9ab29f114c4043a527773dbdef210859
Gabytto <gabyttox@gmail.com>
2025-12-11 23:50:45 -0300

    primer build

--- Assets/Scripts/consumirPota.cs
+++ Assets/Scripts/consumirPota.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class consumirPota : MonoBehaviour
 {
+    public AudioClip sonidoConsumo;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            if (sonidoConsumo != null)
+            {
+                // Reproduce el sonido una sola vez, creando un objeto temporal en la posición.
+                AudioSource.PlayClipAtPoint(sonidoConsumo, transform.position);
+            }
             gameObject.SetActive(false);
         }
     }
